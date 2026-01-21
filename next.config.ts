eb3a2321b1e4db64592998ccdc2a5cecc046a137
Hồ A Mua <homua87@gmail.com>
2026-01-21 14:54:29 +0000

    {   "logName": "projects/studio-101030541-d86dc/logs/firebaseapphosting.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    allowedNextBundlerReactClientConnections: [
+      '*.us-central1.hosted.app',
+    ],
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
