b7db26bc2b8e246807a29d75d767efc63251773a
Hồ A Mua <homua87@gmail.com>
2026-01-21 00:44:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-101030541-d86dc",
+  "appId": "1:477110898495:web:857c68263a0d3fac6a8d8a",
+  "apiKey": "AIzaSyBJXqm_4DEd55HSZ3XTD8WdmyFyqwU9rDM",
+  "authDomain": "studio-101030541-d86dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "477110898495"
+};
